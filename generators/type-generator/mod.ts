5842a5cdd8b7bbbb26f93002e9b27fd75679f881
Niklas Limberg <n.limberg@shopware.com>
2023-05-19 14:14:17 +0200

    Move type-generator to generators folder

---
+++ generators/type-generator/mod.ts
@@ -0,0 +1,11 @@
+import { generateEventTypes } from "./generateEventTypes.ts";
+import { generateEntityTypes } from "./generateEntities.ts";
+
+Deno.writeTextFileSync(
+  "../types/webhooks.d.ts",
+  generateEventTypes("./events.json", "./entity-schema.json"),
+);
+Deno.writeTextFileSync(
+  "../types/entitys.d.ts",
+  generateEntityTypes("./entity-schema.json"),
+);
--- packages/type-generator/mod.ts
+++
@@ -1,6 +0,0 @@
-import { generateEventTypes } from "./generateEventTypes.ts";
-import { generateEntityTypes } from "./generateEntities.ts";
-
-
-Deno.writeTextFileSync("../types/webhooks.d.ts", generateEventTypes("./events.json", "./entity-schema.json"));
-Deno.writeTextFileSync("../types/entitys.d.ts", generateEntityTypes("./entity-schema.json"));
